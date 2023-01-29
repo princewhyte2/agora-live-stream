31cd84a846cf8e771ae2d66ae81170287b159ee2
Prince whyte dabotubo <princewhyte02@gmail.com>
2023-01-29 20:49:04 +0100

    feat:video frame

--- js/room_rtc.js
+++ js/room_rtc.js
@@ -71,16 +71,15 @@ let joinStream = async () => {
   document.getElementsByClassName("stream__actions")[0].style.display = "flex"
 
   try {
-    localTracks = await AgoraRTC
-      .createMicrophoneAndCameraTracks
-      // {},
-      // {
-      //   encoderConfig: {
-      //     width: { min: 640, ideal: 1920, max: 1920 },
-      //     height: { min: 480, ideal: 1080, max: 1080 },
-      //   },
-      // },
-      ()
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks(
+      {},
+      {
+        encoderConfig: {
+          width: { min: 640, ideal: 1920, max: 1920 },
+          height: { min: 480, ideal: 1080, max: 1080 },
+        },
+      },
+    )
 
     let player = `<div class="video__container" id="user-container-${uid}">
                     <div class="video-player" id="user-${uid}"></div>

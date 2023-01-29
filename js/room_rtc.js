5a63e614426fe29090a9111116ba632c82fd331e
Prince whyte dabotubo <princewhyte02@gmail.com>
2023-01-29 19:39:52 +0100

    feat:update

--- js/room_rtc.js
+++ js/room_rtc.js
@@ -37,7 +37,7 @@ let sharingScreen = false
 let joinRoomInit = async () => {
   try {
     rtmClient = await AgoraRTM.createInstance(APP_ID)
-    // await rtmClient.login({ uid, token })
+    await rtmClient.login({ uid, token })
 
     await rtmClient.addOrUpdateLocalUserAttributes({ name: displayName })
 
